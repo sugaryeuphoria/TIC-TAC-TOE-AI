1347f6781666f1593aeb0085995e3cd1a19b5123
Pooja Verma <pooja32verma@gmail.com>
2024-01-30 15:12:32 -0800

    Check rows, columns, and diagonals for a winning combination

--- TIC_TAC_TOE_AI.java
+++ TIC_TAC_TOE_AI.java
@@ -67,5 +67,12 @@ private static void makeMove(char[][] board, char player) {
 }
 // Function to check if there is a winner
 private static boolean isWinner(char[][] board, char player) {
+    // Check rows, columns, and diagonals for a winning combination
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
 }
     
