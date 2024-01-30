a522c7a0f80f8c4a3131e48666daf2c0222a6ba8
Pooja Verma <pooja32verma@gmail.com>
2024-01-30 15:09:31 -0800

    Function to display the game board

--- TIC_TAC_TOE_AI.java
+++ TIC_TAC_TOE_AI.java
@@ -35,5 +35,16 @@ public class TicTacToeAI {
      currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
 }
+// Function to display the game board
+private static void displayBoard(char[][] board) {
+    System.out.println("-------------");
+    for (int i = 0; i < 3; i++) {
+        System.out.print("| ");
+        for (int j = 0; j < 3; j++) {
+            System.out.print(board[i][j] + " | ");
+        }
+        System.out.println("\n-------------");
+    }
+}
 }
     }
